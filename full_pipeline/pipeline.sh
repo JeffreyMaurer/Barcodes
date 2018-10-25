a26c8cd385e8f6b0c12d67b3d6a2a54f3290e110
JeffreyMaurer <maurerj@bu.edu>
2018-10-25 19:59:02 -0400

    Add files via upload

---
+++ full_pipeline/pipeline.sh
@@ -0,0 +1,6 @@
+#length1, length2, mem
+
+3_Paluncec_Generation/parallel/mem/GenerateCodeBooks.exe
+4_Concatenate/concatenate.exe
+5_Remove_Paluncec_Twins/parallel/TossCodeBooks.exe
+6_Biochem_Parse_Paluncec/ProcessCodeBooks.exe
